71a8f472dfc4c197a602a72aba4683e022bdfadd
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-10-17 22:31:47 +0200

    Started using templates

--- vms.ps1
+++ vms.ps1
@@ -20,6 +20,25 @@ function Get-LinuxVMs {
     Out-AsTable($list)
 }
 
+function Get-SortedVMs {
+    $sorted = Get-RunningVms | Sort-Object -Property Name
+    return $sorted
+}
+
+function Get-TemplateFromVM($vm) {
+    
+    $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+    
+# I have tried exporting templates directly from the VM in portal, always throws an error that parameteres aint valid
+
+    New-AzResourceGroupDeployment `
+        -ResourceGroupName "myResourceGroup" `
+        -TemplateFile "C:\Users\Milos\Desktop\GitHub interview prep\template.json" `
+        -TemplateParameterFile "C:\Users\Milos\Desktop\GitHub interview prep\parameters.json"
+        
+    (Get-AzVm -ResourceGroupName "myResourceGroup").name
+}
+
 function Get-CPUUsage($location) {
     if ($null -eq $location -or $location -eq "") {
         Write-Host "No location provided"
@@ -38,7 +57,12 @@ Out-AsTable(Get-RunningVms)
 
 # 5)
 $location = Read-Host "Type in a location to search for"
+Write-Host "Listing vCPU usages for" $location"..."
 Out-AsTable(Get-CPUUsage($location))
 
 # 6)
+Write-Host "Listing running Linux VMs..."
 Get-LinuxVMs
+
+$vms = Get-RunningVms
+Get-TemplateFromVM($vms[0])
